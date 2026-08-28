e0d67ab01d7ed639e6d81359eadef7a7beb76112
Uzair Khan <uzairkhan60621@gmail.com>
2026-08-28 21:05:26 +0500

    Adding the Navbar in the main file App.jsx

--- 1st-basics/src/App.jsx
+++ 1st-basics/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
     <>
-    
+      <Navbar />
+
+      <h1>Welcome to TechNova</h1>
+      <p>My first React application.</p>
     </>
   )
 }
