9b57ee279869c0b8e88b5856a6138a4bbce53ff4
Uzair Khan <uzairkhan60621@gmail.com>
2026-08-29 14:35:16 +0500

    Adding code to make form to get inputs like username and age etc

--- 1st-basics/src/App.jsx
+++ 1st-basics/src/App.jsx
@@ -6,8 +6,61 @@ function App() {
     <>
       <Navbar />
 
-      <h1>Welcome to TechNova</h1>
-      <p>My first React application.</p>
+      <form className="form-container">
+        {/* Text Input */}
+        <div className="form-group">
+          <label htmlFor="userName">Name</label>
+          <input 
+            type="text" 
+            id="userName" 
+            name="userName" 
+            placeholder="Enter your name" 
+            required 
+          />
+        </div>
+
+        {/* Number Input */}
+        <div className="form-group">
+          <label htmlFor="userAge">Age</label>
+          <input 
+            type="number" 
+            id="userAge" 
+            name="userAge" 
+            min="0" 
+            placeholder="Enter your age" 
+          />
+        </div>
+
+        {/* Radio Buttons for Single Choice */}
+        <div className="form-group-status">
+          <span className="label-title">Status</span>
+          
+          <div className="radio-option">
+            <input 
+              style={{accentColor:'green'}}
+              type="radio" 
+              id="statusOnline" 
+              name="status" 
+              value="online" 
+              defaultChecked 
+            />
+            <label htmlFor="statusOnline">Online</label>
+          </div>
+
+          <div className="radio-option">
+            <input 
+              style={{accentColor:'gray'}}
+              type="radio" 
+              id="statusOffline" 
+              name="status" 
+              value="offline" 
+            />
+            <label htmlFor="statusOffline">Offline</label>
+          </div>
+        </div>
+      </form>
+
+
     </>
   )
 }
